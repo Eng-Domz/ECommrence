f392633ceb7e2e0d0246c80b12e57cfade037378
Eng-Domz <aaggffetouh@gmail.com>
2025-03-25 04:04:18 +0200

    final

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/ECommrence/', // 🚨 Case-sensitive
   plugins: [react()],
-  base: '/ECommrence/',
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
+    emptyOutDir: true
   }
 })
